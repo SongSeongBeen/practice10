c789fac1d4ee04bf7b9399b3a400b912c8980607
송성빈 <aktm1004@naver.com>
2022-06-20 12:36:26 +0900

    ch19 메모리 할당위치

---
+++ src/com/javaex/ch19/TestMethod.java
@@ -0,0 +1,18 @@
+package com.javaex.ch19;
+
+public class TestMethod {
+	
+	int num; //힙메모리
+	
+	void aaa() {
+		System.out.println("aaa() 호출 한글이 한글 지워지");
+	}
+							//main()함수의 지역변수 args 지역변수 스택메모리
+	public static void main(String[] args) {
+		TestMethod a1 = new TestMethod();
+		a1.aaa(); //aaa() 메서드 영역
+		//a1 스택 메모리
+		TestMethod a2 = new TestMethod();
+		a2.aaa();
+	}
+}

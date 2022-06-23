e2f165c87246060d7f5abef0af6b1ff4843dfecc
송성빈 <aktm1004@naver.com>
2022-06-23 14:35:06 +0900

    ch20

--- src/com/javaex/ch19/TestMethod.java
+++ src/com/javaex/ch19/TestMethod.java
@@ -2,7 +2,7 @@ package com.javaex.ch19;
 
 public class TestMethod {
 	
-	int num; //힙메모리
+	int num; //전역변수(객체변수)
 	
 	void aaa() {
 		System.out.println("aaa() 호출 한글이 한글 지워지");
@@ -10,9 +10,11 @@ public class TestMethod {
 							//main()함수의 지역변수 args 지역변수 스택메모리
 	public static void main(String[] args) {
 		TestMethod a1 = new TestMethod();
+		//a1의 num 힙메모리
 		a1.aaa(); //aaa() 메서드 영역
 		//a1 스택 메모리
 		TestMethod a2 = new TestMethod();
+		//a2의 num 힙메모리
 		a2.aaa();
 	}
 }

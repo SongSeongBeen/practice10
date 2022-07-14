7fba98f52c91ff91892e6da8a8926163b0d722be
송성빈 <aktm1004@naver.com>
2022-07-14 17:27:07 +0900

    Object, EqualsTest

--- src/com/javaex/ch22/CarTest.java
+++ src/com/javaex/ch22/CarTest.java
@@ -2,11 +2,11 @@ package com.javaex.ch22;
 
 public class CarTest {
 	public static void main(String[] args) {
-		Car aiCar = new AICar();
-		aiCar.run();
-		System.out.println("=============");
-		
-		Car manualCar = new ManualCar();
-		manualCar.run();
+//		Car aiCar = new AICar();
+//		aiCar.run();
+		System.out.println(System.getProperty("user.dir"));
+//		
+//		Car manualCar = new ManualCar();
+//		manualCar.run();
 	}
 }
